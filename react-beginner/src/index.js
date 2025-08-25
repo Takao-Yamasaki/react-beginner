1dbef85b0750406cccf9bab1ab9cb24de957f346
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-08-25 23:41:13 +0900

    add:スプレッド構文

--- react-beginner/src/index.js
+++ react-beginner/src/index.js
@@ -150,12 +150,51 @@
 /**
  * オブジェクトの省略記法
  */
-const name = "じゃけえ"
-const age = 31;
+// const name = "じゃけえ"
+// const age = 31;
 
 // プロパティ名と変数名が同一の場合は省略可能
-const myProfile = {
-  name,
-  age
-};
-console.log(myProfile);
+// const myProfile = {
+//   name,
+//   age
+// };
+// console.log(myProfile);
+
+/**
+ * スプレッド構文...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// (2) [1, 2]
+// console.log(arr1);
+// 1 2
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// 配列をまとめて受け取る
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー・結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// この書き方だと破壊的変更になってしまう
+// const arr8 = arr4;
+// arr4[0] = 100;
+// console.log(arr4);
+// console.log(arr8);
